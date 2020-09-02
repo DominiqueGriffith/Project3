b8018f28515b03190427c226b856aac72ac081ff
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-09-02 18:40:05 -0400

    Attempted to enable add book functionality

--- samepage/src/components/AddBookBtn.js
+++ samepage/src/components/AddBookBtn.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../components/utils/API";
+import LoggedIn from "../pages/loggedIn"
 import { Container, Col, Row, Button } from "reactstrap";
 
-function AddBookBtn (props){
-    return(
-<button onClick={props.onClick}> Add Book{console.log(props + "Clicked!!")}</button>
-    );
+class AddBookBtn extends Component  {
+
+state = {
+    savedBooks: [],
+}
+
+componentDidMount() {
+    API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        .catch(err => console.error(err));
+}
+
+// handleSave = book => {
+
+//     if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
+//         API.deleteBook(book._id)
+//             .then(deletedBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deletedBook._id) }))
+//             .catch(err => console.error(err));
+//     } else {
+//         API.saveBook(book)
+//             .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook]) }))
+//             .catch(err => console.error(err));
+            
+
+//     }
+// }
+render() {
+    return (
+        <button o className="btn badge-pill btn-outline-warning mt-3 ml-3" >
+    
+    </button>
+    )
+
+
+    }
 }
 
 export default AddBookBtn;
